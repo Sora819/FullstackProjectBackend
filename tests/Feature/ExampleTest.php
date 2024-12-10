299aff3077737609dcb08a014524aece62ab3407
Pisti <sora.pisti@gmail.com>
2024-12-10 03:14:03 +0100

    Test workflow

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,7 +12,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get("http://127.0.0.1:8000/api/leaderboard");
 
         $response->assertStatus(200);
     }
